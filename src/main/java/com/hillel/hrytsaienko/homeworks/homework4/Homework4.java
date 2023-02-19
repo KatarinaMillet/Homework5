bf1dfa25dcb0dbcfca26aba014db58b3ff0d3358
Ekaterina Hrytsaienko <solnce2013@gmail.com>
2023-02-19 21:56:41 +0200

    2

--- src/main/java/com/hillel/hrytsaienko/homeworks/homework4/Homework4.java
+++ src/main/java/com/hillel/hrytsaienko/homeworks/homework4/Homework4.java
@@ -2,5 +2,7 @@ package com.hillel.hrytsaienko.homeworks.homework4;
 
 public class Homework4 {
 
-    public static final String AAA = "test";
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
